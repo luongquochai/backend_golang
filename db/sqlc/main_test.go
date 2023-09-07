4aae15a8fc21ae0e806af7b9b004ea1297ad4031
luongquochai <luongquochai0707@gmail.com>
2023-09-07 22:49:39 +0700

    Update load config file from viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,20 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@127.0.0.1:5432/simple_bank?sslmode=disable"
+	"github.com/luongquochai/backend_golang/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
 
-	testDB, err := sql.Open(dbDriver, dbSource)
+	testDB, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
